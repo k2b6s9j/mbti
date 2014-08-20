30dd6b1ef0f2d65918362ec02331017a039bc5fc
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-08-20 00:34:27 -0600

    Added methods to detect the personality trait.

--- lib/mbti/type.rb
+++ lib/mbti/type.rb
@@ -12,6 +12,86 @@ module Mbti
       @judging_function = judging_function
       @lifestyle = lifestyle
     end
+    
+    # Returns true if the type has a tendancy towards extraversion.
+    # Returns false if the type does not.
+    def extraverted?
+      if @attitude < 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards introversion.
+    # Returns false if the type does not.
+    def introverted?
+      if @attitude > 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards intuition.
+    # Returns false if the type does not.
+    def intuitive?
+      if @perceiving_function < 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards observation.
+    # Returns false if the type does not.
+    def observant?
+      if @perceiving_function > 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards thinking.
+    # Returns false if the type does not.
+    def thinking?
+      if @judging_function < 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards feeling.
+    # Returns false if the type does not.
+    def feeling?
+      if @judging_function > 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards judging.
+    # Returns false if the type does not.
+    def judging?
+      if @lifestyle < 0
+        true
+      else
+        false
+      end
+    end
+    
+    # Returns true if the type has a tendancy towards perceiving.
+    # Returns false if the type does not.
+    def perceiving?
+      if @lifestyle > 0
+        true
+      else
+        false
+      end
+    end
 
   end
 
