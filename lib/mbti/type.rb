e1528017d70c729d47a07a00696758c2e12a5d0b
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-08-20 00:15:35 -0600

    Create basic class.

--- lib/mbti/type.rb
+++ lib/mbti/type.rb
@@ -1,6 +1,17 @@
 module Mbti
 
+  # An instance of this class will provide proper representation of a single Myers-Briggs personality type.
   class Type
+    
+    attr_accessor :attitude, :perceiving_function, :judging_function, :lifestyle
+    
+    # Creates an instance of the Type class
+    def initialize(attitude, perceiving_function, judging_function, lifestyle)
+      @attitude = attitude
+      @perceiving_function = perceiving_function
+      @judging_function = judging_function
+      @lifestyle = lifestyle
+    end
 
   end
 
