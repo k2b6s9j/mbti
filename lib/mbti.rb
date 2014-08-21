2ed0acefa8a241d2723ce36912e16b1ca6e59648
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-08-20 23:50:41 -0600

    Add a basic description for this entire module.

--- lib/mbti.rb
+++ lib/mbti.rb
@@ -2,6 +2,7 @@ Dir[File.dirname(__FILE__) + '/mbti/**/*.rb'].each do |file|
   load file
 end
 
+# Contains all the modules, classes, and methods needed to correctly represent and interact with a Myer-Briggs Type Indicator.
 module Mbti
 
 end
