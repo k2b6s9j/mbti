5dc7e7d0f2a4123f705879d99f9b7f8e41a5f332
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-08-21 23:57:22 -0600

    Basic test.

---
+++ spec/type_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Mbti::Type do
+
+  before :each do
+    @type = Mbti::Type
+  end
+
+  it do
+    expect(@type).to be
+  end
+
+  it do
+    expect(@type).to be_truthy
+  end
+
+  it do
+    expect(@type).to_not be_falsey
+  end
+
+  it do
+    expect(@type).to_not be_nil
+  end
+
+  it do
+    expect(@type).to be_a Class
+  end
+
+end
