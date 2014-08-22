99e9f49156cd3d72aa14e0e7df59f818713ef13e
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-08-22 00:03:46 -0600

    Correct sytax.

--- lib/mbti/type.rb
+++ lib/mbti/type.rb
@@ -23,62 +23,62 @@ module Mbti
     # @return [Mbti::Type] the instance of {Mbti::Type} created
     def initialize(attitude, perceiving_function, judging_function, lifestyle)
       case attitude
-        when attitude.is_a? Numeric
+        when(attitude.is_a? Numeric)
           @attitude = attitude
-        when attitude == :introvert
+        when(attitude == :introvert)
           @attitude = 1
-        when attitude == :i
+        when(attitude == :i)
           @attitude = 1
-        when attitude == :extrovert
+        when(attitude == :extrovert)
           @attitude = -1
-        when attitude == :e
+        when(attitude == :e)
           @attitude = -1
         else
           raise StandardError, "Improper Attitude Trait Parameter: #{attitude}"
       end
 
       case perceiving_function
-        when perceiving_function.is_a? Numeric
+        when(perceiving_function.is_a? Numeric)
           @perceiving_function = perceiving_function
-        when perceiving_function == :intuitive
+        when(perceiving_function == :intuitive)
           @perceiving_function = -1
-        when perceiving_function == :n
+        when(perceiving_function == :n)
           @perceiving_function = -1
-        when perceiving_function == :observant
+        when(perceiving_function == :observant)
           @perceiving_function = 1
-        when perceiving_function == :observing
+        when(perceiving_function == :observing)
           @perceiving_function = 1
-        when perceiving_function == :s
+        when(perceiving_function == :s)
           @perceiving_function = 1
         else
           raise StandardError, "Improper Perceiving Function Trait Parameter: #{perceiving_function}"
       end
 
       case judging_function
-        when judging_function.is_a? Numeric
+        when(judging_function.is_a? Numeric)
           @judging_function = judging_function
-        when judging_function == :thinking
+        when(judging_function == :thinking)
           @judging_function = -1
-        when judging_function == :t
+        when(judging_function == :t)
           @judging_function = -1
-        when judging_function == :feeling
+        when(judging_function == :feeling)
           @judging_function = 1
-        when judging_function == :f
+        when(judging_function == :f)
           @judging_function = 1
         else
           raise StandardError, "Improper Judging Function Trait Parameter: #{judging_function}"
       end
 
       case lifestyle
-        when lifestyle.is_a? Numeric
+        when(lifestyle.is_a? Numeric)
           @lifestyle = lifestyle
-        when lifestyle == :judging
+        when(lifestyle == :judging)
           @lifestyle = -1
-        when lifestyle == :j
+        when(lifestyle == :j)
           @lifestyle = -1
-        when lifestyle == :perceiving
+        when(lifestyle == :perceiving)
           @lifestyle = 1
-        when lifestyle == :p
+        when(lifestyle == :p)
           @lifestyle = 1
         else
           raise StandardError, "Improper Lifestyle Trait Parameter: #{lifestyle}"
